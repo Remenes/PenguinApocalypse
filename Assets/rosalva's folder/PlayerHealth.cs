df5553127b6d4579a792009a3e1af573fd49add1
rosalvarodriguez <rosalvr2@uci.edu>
2017-05-01 12:40:23 -0700

    added my folder

---
+++ Assets/rosalva's folder/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Player is dead");
+        }
+    }
+
+    public void RestoreHealth(int health)
+    {
+        currentHealth += health;
+        if (currentHealth >= maxHealth)
+            currentHealth = maxHealth;
+    }
+
+	//void Update () { TakeDamage(20); }
+}
---
+++ Assets/rosalva's folder/PlayerShooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject projectile;
+    private Rigidbody2D shot_rg;
+    public float projectile_speed = 150;
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+
+    void Fire()
+    {
+        GameObject shot = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        //give initial veloctiy
+        shot_rg = shot.GetComponent<Rigidbody2D>();
+        shot_rg.velocity = new Vector3(0, projectile_speed, 0);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            InvokeRepeating("Fire", 0.00001f, .02f);
+        if (Input.GetKeyUp(KeyCode.Space))
+            CancelInvoke("Fire");
+    }
+}
