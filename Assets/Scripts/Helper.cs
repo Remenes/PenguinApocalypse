55bf85b5fb33a88e316f3b313b4b41add34a8270
Roger <roger@dalkefamily.com>
2017-05-16 15:43:20 -0700

    Added generic health class

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -2,6 +2,8 @@
 
 public class Helper : MonoBehaviour
 {
+    public delegate void VoidDelegate();
+
     public static Vector3 Vector2toVector3(Vector2 vector, float zOffset = 0)
     {
         return new Vector3(vector.x, vector.y, zOffset);
