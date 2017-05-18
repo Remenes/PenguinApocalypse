2af6909494398c2d72ad89c369f23fa56a9af637
rosalvarodriguez <rosalvr2@uci.edu>
2017-05-17 18:07:08 -0700

    changes plz ignore

---
+++ Assets/rosalva's folder/ProjectileDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDamage : MonoBehaviour {
+
+    float damage = 10;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+            coll.gameObject.GetComponent<Health>().takeDamage(damage);
+        Destroy(gameObject);
+    }
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
